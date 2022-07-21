a8ba9b56cf1cc0474882ac770dd021d12eeba6c1
Phubordee Romin <phubordee.r@ku.th>
2022-07-22 00:26:39 +0700

    Add annotation, create value.properties bean.xml and edit main class

--- src/main/java/atm/DataSourceDB.java
+++ src/main/java/atm/DataSourceDB.java
@@ -1,8 +1,13 @@
 package atm;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
+@Primary
 public class DataSourceDB implements DataSource {
 
     /**
@@ -14,6 +19,7 @@ public class DataSourceDB implements DataSource {
         customers.put(1,new Customer(1,"Peter",1234,1000));
         customers.put(2,new Customer(2,"Katherine",2345,2000));
         customers.put(3,new Customer(3,"Chris",3456,3000));
+        customers.put(27,new Customer(27,"Phubordee",2022,99000));
 
         return customers;
     }
